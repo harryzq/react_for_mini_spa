26e65774b004e2c80c63f5f2d3f9454ea809fecf
qiangzhang <qiang.zhang@garena.cn>
2019-12-13 19:37:11 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import "./css/App.scss";
+import utils from "./utils";
+import Modal from "react-responsive-modal";
+import axios from "axios";
+// headers: {
+//   'X-CSRFToken': utils.getCookie('csrftoken')
+// }
 function App() {
+  // 初始化数据
+  const [lang, setLang] = useState(utils.getLanguage());
+  const [accessToken, setAccessToken] = useState(utils.getAndStoreParams(lang));
+  useEffect(() => {
+    // 语言设置
+    document.getElementsByTagName("html")[0].setAttribute("lang", lang);
+  });
+
+  // 获取用户数据
+  const [iniInfo, setInitInfo] = useState({});
+  const getInitInfo = (accessToken,lang) => {
+    const http = axios.create({
+      //创建axios实例，在这里可以设置请求的默认配置
+      timeout: 10000,
+      withCredentials: true
+    });
+    http.defaults.headers.post["X-CSRFToken"] =
+      utils.getCookie("csrftoken") || "csrftoken";
+    http.post("http://119.28.12.109:3100/mock/56/api/spin").then((res, rej) => {
+      setInitInfo(value=>({
+        ...value,
+        data:res.data.rewards
+      }))
+      
+    });
+  };
+  useEffect(() => {
+    getInitInfo(accessToken,lang)
+  },[]);
+
+  const [modalOpen, setModalOpen] = useState(false);
+  // modal控制
+  const onCloseModal = () => {
+    setModalOpen(false);
+  };
+  const onOpenModal = () => {
+    setModalOpen(true);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onOpenModal}>Open modal</button>
+      <Modal open={modalOpen} onClose={onCloseModal} center>
+        <h2>Simple {iniInfo} modal</h2>
+      </Modal>
     </div>
   );
 }
---
+++ src/utils/index.js
@@ -0,0 +1,114 @@
+/* eslint-disable */
+const removeUrlParams = name => {
+  var query = window.location.search.substr(1);
+  if (query.indexOf(name) > -1) {
+    var obj = {};
+    var arr = query.split("&");
+    for (var i = 0; i < arr.length; i++) {
+      arr[i] = arr[i].split("=");
+      obj[arr[i][0]] = arr[i][1];
+    }
+    delete obj[name];
+    var url = JSON.stringify(obj)
+      .replace(/[\"\{\}]/g, "")
+      .replace(/\:/g, "=")
+      .replace(/\,/g, "&");
+    var http = "https:" == document.location.protocol ? "https://" : "http://";
+    window.history.replaceState(
+      {},
+      0,
+      http + window.location.host + window.location.pathname + "?" + url
+    );
+  }
+};
+
+const getLanguage = () => {
+  let acceptableLang = [
+    "ar",
+    "en",
+    "es",
+    "fr",
+    "id",
+    "pt",
+    "ru",
+    "th",
+    "tr",
+    "vi",
+    "zh",
+    "ko",
+    "ja",
+    "ar",
+    "de",
+    "jv",
+    "ms"
+  ];
+  let query = window.location.search.substring(1);
+  let lang, vars;
+  if (query) {
+    vars = query.split("&");
+    lang = vars[0].split("=")[1];
+  }
+
+  if (lang) {
+    return lang;
+  } else {
+    lang = (navigator.language || navigator.browserLanguage).toLowerCase();
+    for (var i = 0; i < acceptableLang.length; i++) {
+      acceptableLang[i] = acceptableLang[i].toLowerCase();
+      if (lang === acceptableLang[i]) {
+        return lang;
+      } else if (lang === "zh-tw") {
+        return "zh";
+      }
+    }
+    return "en";
+  }
+};
+
+const getCookie = name => {
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i].trim();
+      if (cookie.substring(0, name.length + 1) === name + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+};
+
+const getTime = (time, config) => {
+  if (!config) config = "UTC";
+  if (config === "UTC") {
+    return new Date(time * 1000);
+  }
+};
+// 用户信息存储
+const getAndStoreParams = lang => {
+  let query = window.location.search.substring(1);
+  let vars, access_token;
+  if (query) {
+    vars = query.split("&");
+    if(vars[1]){
+      access_token = vars[1].split("=")[1];
+    }
+    
+  }
+
+  if (!access_token) {
+    access_token = window.sessionStorage.getItem("AccessToken") || "";
+  }
+  access_token && window.sessionStorage.setItem("AccessToken", access_token);
+  lang && window.sessionStorage.setItem("Lang", lang);
+  return access_token;
+};
+export default {
+  getLanguage,
+  getCookie,
+  removeUrlParams,
+  getTime,
+  getAndStoreParams
+};
